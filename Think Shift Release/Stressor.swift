fe0113a17ec5e5e8ae66ba67b6a37f38f7151927
Ignacio Zunino <zignacio@gmail.com>
2017-07-26 10:01:08 -0300

    model definitions and helper methods

--- Think Shift Release/Stressor.swift
+++ Think Shift Release/Stressor.swift
@@ -18,4 +18,19 @@ final class Stressor: Object {
     dynamic var title: String?
     dynamic var completed: Bool = false
     dynamic var dateCreated: Date = Date()
+    
+    //think
+    dynamic var thinkThoughts: String?
+    dynamic var thinkInnerWisdom: String?
+    dynamic var thinkActionStep: String?
+    dynamic var thinkBetterFeeling: String?
+    
+    //shift
+    dynamic var shiftBoundariesNotTalkWith: String?
+    dynamic var shiftBoundariesDoTalkWith: String?
+    
+    //release
+    dynamic var releaseIntention: String?
+    dynamic var releaseAffirmation: String?
+    
 }
--- Think Shift Release/User.swift
+++ Think Shift Release/User.swift
@@ -9,6 +9,17 @@
 import Foundation
 import RealmSwift
 
+final class StringObject: Object {
+
+    dynamic var value: String?
+    
+    convenience init(internalValue: String) {
+        self.init()
+        self.value = internalValue
+    }
+    
+}
+
 final class User: Object {
     
     let stressors = List<Stressor>()
@@ -17,5 +28,33 @@ final class User: Object {
         return Array( self.stressors.filter("completed = false") )
     }
     
+    let audioPaths = List<StringObject>()
+    let imagesPaths = List<StringObject>()
+    let videoPaths = List<StringObject>()
+    let quotes = List<Quote>()
+    
+    private func addMedia(list: List<StringObject>, value: String) {
+        Database.shared.save {
+            list.append(StringObject(internalValue: value))
+        }
+    }
+
+    func addAudio(audioPath: String) {
+        self.addMedia(list: self.audioPaths, value: audioPath)
+    }
+
+    func addVideo(videoPath: String) {
+        self.addMedia(list: self.videoPaths, value: videoPath)
+    }
+
+    func addImage(imagePath: String) {
+        self.addMedia(list: self.imagesPaths, value: imagePath)
+    }
+
+    func addQuote(quote: Quote) {
+        Database.shared.save {
+            self.quotes.append(quote)
+        }
+    }
     
 }
