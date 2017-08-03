94440ac0f2ef8565e7ca26e549f2fb913478e555
Ricardo Hernandez <a.ricardohg@gmail.com>
2017-08-03 11:59:50 -0500

    added elements for deleating

--- Think Shift Release/HomeViewController.swift
+++ Think Shift Release/HomeViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var topOverlay: UIView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var selectLabel: UILabel!
+    
+    fileprivate var isDeleting = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Think, Shift, Release"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(edit(_:)))
+        self.editButton.title = "Edit"
     }
 
+    
+    private func toogleDeleteMode() {
+        
+        self.isDeleting = !self.isDeleting
+       // self.collectionView.reloadData()
+        self.editButton.title = self.isDeleting ? "Done" : "Edit"
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.topOverlay.alpha = self.isDeleting ? 0.6 : 0
+            self.deleteButton.alpha = self.isDeleting ? 1 : 0
+            self.selectLabel.alpha = self.isDeleting ? 1 : 0
+        }) { (complete) in
+            
+        }
+    }
+
+    
 
     // MARK: - User Action
     
@@ -24,7 +48,9 @@ class HomeViewController: UIViewController {
     }
     
     @IBAction func edit(_ sender: Any?) {
-        print("addItem")
+        self.toogleDeleteMode()
     }
 
+    @IBAction func deleteAction(_ sender: Any) {
+    }
 }
