a6aca325f58b19fa856bb3626d59d053d86fc846
Ricardo Hernandez <a.ricardohg@gmail.com>
2017-08-15 17:14:49 -0500

    added add buttons in quotes and music

---
+++ Think Shift Release/MusicViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MusicViewController.swift
+//  Think Shift Release
+//
+//  Created by ricardo hernandez  on 8/15/17.
+//  Copyright © 2017 Zilliance. All rights reserved.
+//
+
+import UIKit
+
+class MusicViewController: UIViewController {
+    
+    private var tableViewController: MusicTableViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.layer.contents = UIImage(named: "shift-bg")?.cgImage
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? MusicTableViewController {
+            self.tableViewController = vc
+        }
+    }
+
+    @IBAction func addTrackAction(_ sender: Any) {
+        self.tableViewController.addItem(self)
+    }
+
+}
