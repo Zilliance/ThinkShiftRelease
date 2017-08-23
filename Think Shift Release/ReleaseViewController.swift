eeeda9d007dbea5229d098c8b6454dab7979e260
Philip Dow <phil@phildow.net>
2017-08-23 09:17:38 -0700

    remove video icon from release section -- we show it on the stressor card

--- Think Shift Release/ReleaseViewController.swift
+++ Think Shift Release/ReleaseViewController.swift
@@ -29,12 +29,10 @@ class ReleaseViewController: UIViewController, ShowsSummary {
     var segment: Int?
     
     var summarySection: ItemSection! = .release
-    private var playbackObserver: NSObjectProtocol?
     
     fileprivate var isSectionCompleted: Bool {
         return !self.affirmationTextView.text.isEmpty && !self.intentionTextView.text.isEmpty && !self.experienceTextView.text.isEmpty
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -102,79 +100,6 @@ class ReleaseViewController: UIViewController, ShowsSummary {
         LearnMoreViewController.present(from: self, text: NSLocalizedString("breathe learn more", comment: ""))
     }
     
-    // MARK: - Videos
-    
-    @IBAction func playVideo(_ sender: Any?) {
-        self.play(animation: .release, completion: nil)
-    }
-    
-    private func play(animation: SectionAnimation, completion: (()->Void)?) {
-        let player = AVPlayer(url: Bundle.main.url(forResource: animation.rawValue, withExtension: "mp4")!)
-        let host = UIStoryboard(name: "Animation", bundle: nil).instantiateInitialViewController() as! SectionAnimationViewController
-        
-        let _ = host.view // force load for access to embedded playerViewController
-        
-        host.playerViewController.player = player
-        host.transitioningDelegate = self
-        host.modalPresentationStyle = .custom
-        host.animation = animation
-        host.delegate = self
-        
-        self.playbackObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { [weak self] (notification) in
-            
-            if let playbackObserver = self?.playbackObserver {
-                NotificationCenter.default.removeObserver(playbackObserver)
-                self?.playbackObserver = nil
-            }
-            
-            if !host.isFullScreen {
-                // Programmatically dismissing when player is full screen causes a crash
-                host.dismiss(animated: true, completion: {
-                    completion?()
-                })
-            }
-        }
-        
-        self.present(host, animated: true, completion: nil)
-        player.play()
-    }
-}
-
-// MARK: -
-
-extension ReleaseViewController: UIViewControllerTransitioningDelegate {
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        switch presented {
-        case is SectionAnimationViewController:
-            return AnimationPresentationController(presentedViewController: presented, presenting: presenting)
-        default:
-            return nil
-        }
-    }
-    
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        switch dismissed {
-        case is SectionAnimationViewController:
-            return AnimationModalTransition(withType: .dismissing)
-        default:
-            return nil
-        }
-    }
-    
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        switch presented {
-        case is SectionAnimationViewController:
-            return AnimationModalTransition(withType: .presenting)
-        default:
-            return nil
-        }
-    }
-}
-
-extension ReleaseViewController: SectionAnimationDelegate {
-    func didDimiss(_ viewController: SectionAnimationViewController) {
-        // noop
-    }
 }
 
 // MARK: -
